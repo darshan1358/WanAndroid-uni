02e4619cf1fb16667366482e02c1e11645e88c74
Darshan1358 <425146022@qq.com>
2019-10-14 09:44:53 +0800

    需要的功能界面创建和首页功能完成

---
+++ app/pages/detailPage/detailPage.vue
@@ -0,0 +1,29 @@
+<template>
+	<web-view :src="url"></web-view>
+</template>
+
+<script>
+	var wv; //计划创建的webview
+	export default {
+		data() {
+			return {
+				title:"",
+				url:"http://www.baidu.com"
+			}
+		},
+		onLoad(par) {
+			this.title = par.title;
+			this.url = par.url
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.content{
+	width: 100%;
+	height: 100%;
+}
+</style>
--- app/pages/index/index.vue
+++ app/pages/index/index.vue
@@ -1,52 +1,143 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+		<swiper class="swiper" autoplay="true" indicator-dots="true" indicator-color="gray">
+			<swiper-item class="swiper-item" v-for="(item, index) in bannerlist" :key="index"><image :src="item.imagePath" mode="widthFix" style="width: 100%;" /></swiper-item>
+		</swiper>
+
+		<view class="list-item" v-for="(item, index) in datalist" :key="index" @click="jumpDetailPage(item)">
+			<view class="list-item-title">{{ item.title }}</view>
+			<view class="list-item-chapter">{{ item.superChapterName +" . "+ item.chapterName }}</view>
 		</view>
+		
+		<view style="visibility:isLoadMore? visible: hidden;">加载更多...</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
+export default {
+	data() {
+		return {
+			isLoadMore:false,
+			pagesize:0,
+			bannerlist: [],
+			datalist: []
+		};
+	},
+	onLoad() {
+		this.loadData();
+	},
+	onReachBottom() {
+		this.isLoadMore = true;
+		this.pagesize++;
+		this.loadListData(this.pagesize);
+		console.log(this.pagesize)
+	},
+	onPullDownRefresh() {
+		this.loadData();
+	},
+	methods: {
+		loadData() {
+			let that = this;
+			//获取首页banner
+			uni.request({
+				url: 'https://www.wanandroid.com/banner/json',
+				success(res) {
+					// console.log('res.data==' + JSON.stringify(res.data.data));
+					that.bannerlist = res.data.data;
+					// console.log('this.bannerlist==' + JSON.stringify(this.bannerlist[0].imagePath));
+				}
+			});
+			//获取首页文章列表
+			uni.request({
+				url: 'https://www.wanandroid.com/article/list/0/json',
+				success(res) {
+					that.datalist = res.data.data.datas;
+					uni.stopPullDownRefresh();
+				},
+				fail() {}
+			});
 		},
-		onLoad() {
-
+		loadListData(pagesize){
+		
+			let that = this;
+			
+			console.log('https://www.wanandroid.com/article/list/'+that.pagesize+'/json');
+			//获取首页文章列表
+			uni.request({
+				url: 'https://www.wanandroid.com/article/list/'+that.pagesize+'/json',
+				success(res) {
+					if(that.pagesize==0){
+						that.datalist = res.data.data.datas;
+					}else{
+						console.log("length=="+that.datalist.length)
+						console.log("datas=="+res.data.data.datas.length)
+						that.datalist = that.datalist.concat(res.data.data.datas);
+						console.log("num=="+that.datalist.length)
+					}
+				},
+				fail() {},
+				complete() {
+					this.isLoadMore = false;
+				}
+			});
 		},
-		methods: {
-
+		jumpDetailPage(item){
+			uni.navigateTo({
+				url:"../detailPage/detailPage?url="+item.link+"&title="+item.title
+			})
 		}
 	}
+};
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+.content {
+	width: 100%;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	background: #eeeeee;
+}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
+.swiper {
+	width: 100%;
+	height: 380upx;
+}
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
+.swiper-item {
+	width: 100%;
+	height: 100%;
+	padding: 0;
+}
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+.list-item {
+	width: 88%;
+	position: relative;
+	display: flex;
+	flex-direction: column;
+	padding: 20upx;
+	margin-top: 20upx;
+	margin-left: 20upx;
+	margin-right: 20upx;
+	border-radius: 16upx;
+	background: white;
+	box-shadow: 5px 5px 10px #C8C7CC;
+}
+
+.list-item-title {
+	width: auto;
+	position: relative;
+	font-size: 30upx;
+	color: $uni-text-color;
+	display: inline-block;
+}
+
+.list-item-chapter {
+	margin-top: 10upx;
+	font-size: 25upx;
+	color: #999999;
+}
+
 </style>
